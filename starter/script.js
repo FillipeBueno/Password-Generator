2ff36b52e3d93638f499980fee1b7f32deb0dde6
Fillipe <you@example.com>
2023-10-29 19:12:28 +0000

    Work Done

---
+++ starter/script.js
@@ -0,0 +1,150 @@
+// Array of special characters to be included in password
+
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
+];
+
+// Array of numeric characters to be included in password
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+
+// Array of lowercase characters to be included in password
+var lowerCasedCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+
+// Array of uppercase characters to be included in password
+var upperCasedCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+];
+
+
+
+// Function for getting a random element from an array
+function getRandom(arr) {
+
+}
+
+// Function to generate password with user input
+function generatePassword() {
+  var userChoice = prompt( "How many characters would you like in you Password?");
+  
+  if (userChoice<=7 || userChoice>=129){
+    
+alert ("you can't choose that number. Please choose between 8 to 128" );
+var userChoice = prompt( "How many characters would you like in you Password?");
+
+
+}
+
+var userChoice=true;
+
+
+  
+}
+var option1 = confirm ("Click OK to confirm including special characters");
+
+  var option2 = confirm ("Click OK to confirm including numeric characters");
+
+  var option3 = confirm ("Click OK to confirm including lowercase characters");
+
+  var option4 = confirm ("Click OK to confirm including uppercase characters");
+
+
+if(!option1&& !option2&& !option3&& !option4)
+{
+  alert  ("you need to choose at least one"); 
+
+
+
+}
+  
+
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector('#generate');
+
+// Write password to the #password input
+function writePassword() {
+  
+  var password = generatePassword();
+  var passwordText = document.querySelector('#password');
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener('click', writePassword);
